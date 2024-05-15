2f3e6fe4011cf0168a5f209c5fd71c6e04028f26
sunghogo <70732249+sunghogo@users.noreply.github.com>
2024-05-15 14:59:17 -0400

    Changed Start() to Awake() since properties need to initialized before refence by other objects, added tmp caching to PropertyBox

--- Assets/Scripts/ColorBox.cs
+++ Assets/Scripts/ColorBox.cs
@@ -11,8 +11,8 @@ public class ColorBox : MonoBehaviour
         set {_rawImage.color = value;} 
     }
 
-    // Start is called before the first frame update
-    void Start()
+    // Need Awake to make sure properties are properly initialized
+    void Awaket()
     {
         _rawImage = GetComponent<RawImage>();
         Color = new Color(1, 1, 1, 1);
--- Assets/Scripts/PropertyBox.cs
+++ Assets/Scripts/PropertyBox.cs
@@ -7,19 +7,62 @@ public class PropertyBox : MonoBehaviour, IPointerClickHandler
     private OpenPanel _openPanel;
     private ClosedPanel _closedPanel;
     private TextMeshProUGUI _propertyTmp;
+    private TextMeshProUGUI _colorTmp;
+    private TextMeshProUGUI _sliderOneTmp;
+    private TextMeshProUGUI _sliderTwoTmp;
+
+    public string PropertyTitleText {
+        get {return _propertyTmp.text;}
+        set {_propertyTmp.text = value;}
+    }
+
+    public string ColorPropertyText {
+        get {return _colorTmp.text;}
+        set {_colorTmp.text = value;}
+    }
+
+    public string SliderOnePropertyText {
+        get {return _sliderOneTmp.text;}
+        set {_sliderOneTmp.text = value;}
+    }
+
+    public string SliderTwoPropertyText {
+        get {return _sliderTwoTmp.text;}
+        set {_sliderTwoTmp.text = value;}
+    }
+
     private ColorBox _colorBox;
     private ColorWheelPanel _colorWheelPanel;
+
     public float OpenedHeight;
     public float ClosedHeight = 35;
     public bool Opened = false;
     public bool Adjusted = false;
 
-    // Start is called before the first frame update
-    void Start()
+    // Need Awake to make sure properties are properly initialized
+    void Awake()
     {
         _openPanel = GetComponentInChildren<OpenPanel>();
         _closedPanel = GetComponentInChildren<ClosedPanel>();
-        _propertyTmp = GetComponentInChildren<TextMeshProUGUI>();
+
+        var tmps = GetComponentsInChildren<TextMeshProUGUI>();
+        foreach (var tmp in tmps) {
+            switch (tmp.name) {
+                case "Property (TMP)":
+                    _propertyTmp = tmp;
+                    break;
+                case "Color (TMP)":
+                    _colorTmp = tmp;
+                    break;
+                case "Slider 1 (TMP)":
+                    _sliderOneTmp = tmp;
+                    break;
+                case "Slider 2 (TMP)":
+                    _sliderTwoTmp = tmp;
+                    break;
+            }
+        }
+
         _colorBox = GetComponentInChildren<ColorBox>();
         _colorWheelPanel = GetComponentInChildren<ColorWheelPanel>();
         OpenedHeight = GetOpenedHeight();
